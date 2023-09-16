76ce7fd1e840b213b655dbd9c857b7cccea1baac
DANIL <example@gmail.ru>
2023-09-16 20:20:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//alert("hello");
+
+
+//const result = confirm("are you here?");
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('как ваше имя?', '');
+answers[1] = prompt('как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
